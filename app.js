52435d051d8dd2ba13313fb5f0372cf6818f002f
adityaPratamaSaputra <suport.aps123@gmail.com>
2022-10-13 14:03:18 +0700

    add homepage

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.render('homepage')
+})
+
+app.listen(3000,()=>{
+    console.log('listening port 3000')
+})
